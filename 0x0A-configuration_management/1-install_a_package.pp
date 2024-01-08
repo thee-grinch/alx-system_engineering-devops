d49476bacf8cc6e397497af57d28b10fe23997ff
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2024-01-08 20:04:47 +0300

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 #this installs flask
-exec {'Install_Flask':
-  command => '/usr/bin/pip3 install Flask==1.0.2',
-  path    => ['/usr/bin']
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }

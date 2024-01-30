8c85761c196b5925e2aaf845cd75a9884dcc8575
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2024-01-30 21:21:45 +0300

    task 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,41 +1,18 @@
 #!/usr/bin/python3
-"""
-this scripts fcetches and uses data from an api
-"""
+"""Exports to-do list information of all employees to JSON format."""
 import json
 import requests
 
-
-def print_tasks():
-    """
-    this script print users details
-    """
-    data_to_be_written = {}
-    for x in range(1, 11):
-        try:
-            tasks_response = requests.get(
-                'https://jsonplaceholder.typicode.com/users/{}/todos'
-                .format(x))
-            user_response = requests.get(
-                'https://jsonplaceholder.typicode.com/users/{}'.format(x))
-            tasks = tasks_response.json()
-            user = user_response.json()
-            username = user.get('username')
-            list_tasks = [
-                {"username": username,
-                 "task": task.get('title'),
-                 "completed": task.get('completed')}
-                for task in tasks]
-            data_to_be_written.update({"{}".format(x): list_tasks})
-            # print(data_to_be_written)
-            # for task in list_tasks:
-            #     print(task)
-        except Exception as e:
-            print('{}'.format(e))
-    filename = 'to_do_all_employees.json'
-    with open(filename, 'w') as jsonfile:
-        json.dump(data_to_be_written, jsonfile)
-
-
 if __name__ == "__main__":
-    print_tasks()
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
+
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            u.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)

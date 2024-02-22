c0adff7f607faf6c86e158df011e67a01b0d5597
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2024-02-22 08:48:38 +0300

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,6 +2,7 @@
 '''this module finds the total number of  subscribers in a subredit'''
 import requests
 
+
 def number_of_subscribers(subreddit):
     """
     Retrieves the number of subscribers for a given subreddit.
@@ -14,9 +15,9 @@ def number_of_subscribers(subreddit):
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "Mordecai"}
-    
+
     response = requests.get(url, headers=headers)
-    
+
     if response.status_code == 200:
         data = response.json()
         return data["data"]["subscribers"]

0c37b886d8cb70da3772d2417ab7be5edc726b4f
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2024-02-26 06:47:27 +0300

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-"""
-Script that queries subscribers on a given Reddit subreddit.
-"""
-
+"""How many subs?
+This function queries subscribers on a given Reddit subreddit"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
+    """Queries the Reddit API and returns
+    the number of subscribers"""
+
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    headers = {
+        "User-Agent": "AppleWebKit:0x16.api.advanced:v1.0.0 \
+                       (by /u/JazzlikeFig4018)"
+    }
+    res = requests.get(url, headers=headers, allow_redirects=False)
+
+    if res.status_code == 404:
         return 0
+    results = res.json().get("data")
+    return results.get("subscribers")

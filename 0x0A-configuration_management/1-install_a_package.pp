a52ff6af7fada3519865907aec59e7913e0170de
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2024-02-27 06:09:27 +0300

    task 2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-#this installs flask
+
+# This installs flask and its dependencies
 package {'flask':
   ensure   => '2.1.0',
+  provider => 'pip3'
+}
+
+package {'werkzeug':
+  ensure   => '2.0.2',
   provider => 'pip3'
 }
